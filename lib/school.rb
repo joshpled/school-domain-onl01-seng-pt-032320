1c88484853a6d7d533978a92d5f6ba225fbbccf7
Joshua Perez Leduc <joshuapleduc@gmail.com>
2020-04-11 23:26:24 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -1,7 +1,7 @@
 class School
   attr_accessor :name, :roster, :student, :grade
+  ROSTER = {}
   def initialize (name)
-    ROSTER = {}
   end
   # def add_student(student,grade)
   #   ROSTER << {@student,@grade}
