c16628aaed845f2b49a23640801ec23d78c7e22b
Joshua Perez Leduc <joshuapleduc@gmail.com>
2020-04-12 01:08:52 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -13,10 +13,7 @@ class School
   def sort
     @roster.each do |grade, students|
       @roster[grade] = students
-      @roster[grade].each do |students|
-        students.sort
-    end
-      end
+    end 
   end
 
 end
